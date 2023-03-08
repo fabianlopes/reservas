4cbecefe2a283c8920317eb0ce99b8ee5e98c338
Fabian Lopes <60930772+fabianlopes@users.noreply.github.com>
2023-03-08 12:08:17 -0400

    aula 08/03

---
+++ frontend/src/componentes/cabecalho.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Cabecalho() {
+  
+  return (
+
+        <div className="App">
+            <header className="Cabecalho">                
+                CABECALHO DO SISTEMA
+            </header>            
+        </div>          
+  );
+}
+
+export default Cabecalho;
