8ffb780907a6bb3af0c12ec9fdb9720585e2234a
fabianlopes <60930772+fabianlopes@users.noreply.github.com>
2023-03-16 13:59:54 -0400

    aula 16.3

--- frontend/src/paginas/salas.js
+++ frontend/src/paginas/salas.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Container, Row, Col } from 'react-bootstrap';
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate  } from "react-router-dom";
 import Cabecalho from '../componentes/cabecalho';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -10,6 +10,7 @@ function Salas() {
   
   const { id } = useParams();
   const [sala, setFormData] = useState([]);
+  const history = useNavigate();
   
   useEffect(() => {
       async function fetchFormData () {
@@ -27,6 +28,20 @@ function Salas() {
       };
       fetchFormData();
     },[id]); 
+
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+      try {
+        
+        console.log('id =' + id);
+        const response = await axios.put(`http://localhost:5000/api/salas/${id}`);
+        setFormData(response.data);      
+        console.log('response ' + response.data);
+      } catch (error) {
+        console.error(error);
+      }
+      history(-1);
+    }
   
   return (    
 
@@ -37,14 +52,15 @@ function Salas() {
      
       <Row>
       
-        <form action="<%= action %>" method="POST">
+        <form onSubmit={handleSubmit}>
           <p>Numero:<input type="text" name="numero" value={sala.numero} /></p>
           <p>Tipo:<input type="text" name="tipo" value={sala.tipo} /></p>
           <p>Capacidade:<input type="text" name="capacidade" value={sala.capacidade} /></p>
           <p>Valor:<input type="text" name="valor" value={sala.valor} /></p>
           <p>Imagem:<input type="text" name="imagem" value={sala.imagem} /></p>
           <p>Descrição:<input type="text" name="descricao" value={sala.descricao} /></p>
-          <input type="submit" value="Salvar" />
+          <input type="submit" name="salvar" value="Salvar" />
+          <input type="submit" name="calcelar" value="Cancelar" />
         </form>
         <img src={sala.imagem} alt="imagem banco"></img>
      
